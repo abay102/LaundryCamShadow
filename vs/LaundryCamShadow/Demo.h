58786b981267eb83e112f6dd9556293506332976
abay102 <86785944+abay102@users.noreply.github.com>
2021-07-22 23:36:30 +0700

    LaundryCamShadow

---
+++ vs/LaundryCamShadow/Demo.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint depthmapShader, shadowmapShader, cubeVBO, cubeVAO, cubeEBO, cube_texture, planeVBO, planeVAO, planeEBO, plane_texture, stexture, stexture2, depthMapFBO, depthMap;
+	GLuint mesincuciVBO, mesincuciVAO, mesincuciEBO, mesincuciTexture, boxVBO, boxVAO, boxEBO, boxTexture;
+	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
+	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+	void Buildmesincuci();
+	void Buildbox();
+	void BuildTexturedPlane();
+	void Drawmesincuci(GLuint shader);
+	void Drawbox(GLuint shader);
+	void DrawTexturedPlane(GLuint shader);
+	void BuildDepthMap();
+	void BuildShaders();
+	void InitCamera();
+	void MoveCamera(float speed);
+	void StrafeCamera(float speed);
+	void RotateCamera(float speed);
+};
+
